b817794b49736ce5e0550169315dda4bb6c097a5
nathanluu <nate1299@me.com>
2021-07-10 22:27:04 -0400

    Updated project card title color

--- components/ProjectsGrid.js
+++ components/ProjectsGrid.js
@@ -5,11 +5,15 @@ export default function ProjectsGrid({projects}) {
     const [currentProject, setCurrentProject] = useState();
 
     return <>
-        <ProjectInfoDialog isOpen={currentProject !== undefined} onClose={() => setCurrentProject(undefined)} project={currentProject}/>
+        <ProjectInfoDialog isOpen={currentProject !== undefined} onClose={() => setCurrentProject(undefined)}
+                           project={currentProject}/>
         <div className="grid gap-5 mx-auto md:grid-cols-2 lg:max-w-none">
             {projects.map((project, index) => <button key={index}
                                                       onClick={() => setCurrentProject(project)}
-                                                   className="flex select-none text-left flex-col transform rounded-xl shadow-lg border border-gray-100 dark:border-gray-800 overflow-hidden hover:opacity-75 focus:ring-2 ring-blue-600 transition-opacity focus:outline-none focus:shadow-xl duration-200">
+                                                      className="flex select-none text-left flex-col transform rounded-xl
+                                                   shadow-lg border border-gray-100 dark:border-gray-800 overflow-hidden
+                                                   hover:opacity-75 focus:ring-2 ring-blue-600 transition-opacity
+                                                   focus:outline-none focus:shadow-xl duration-200">
                 <div className="flex-shrink-0">
                     <img className="h-56 w-full object-cover pointer-events-none"
                          src={project.image}
@@ -17,7 +21,7 @@ export default function ProjectsGrid({projects}) {
                 </div>
                 <div className="flex-1 bg-white dark:bg-gray-800 p-6 flex flex-col justify-between">
                     <div className="flex-1">
-                        <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                        <h3 className="text-xl font-semibold text-gray-800 dark:text-white">
                             {project.title}
                         </h3>
                         <p className="mt-1 text-base leading-6 text-gray-500 dark:text-gray-400">
